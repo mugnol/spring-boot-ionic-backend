6b10c15ffa3ec0f596d775f43b8b717c537bd78a
= <=>
2020-01-10 18:39:40 -0300

    Page

--- src/main/java/com/mugnol/cursomc/services/CategoriaService.java
+++ src/main/java/com/mugnol/cursomc/services/CategoriaService.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.mugnol.cursomc.domain.Categoria;
-import com.mugnol.cursomc.dto.CategoriaDTO;
 import com.mugnol.cursomc.repositories.CategoriaRepository;
 import com.mugnol.cursomc.services.exception.DataIntegrityException;
 import com.mugnol.cursomc.services.exception.ObjectNotFoundException;
@@ -50,5 +52,11 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
-
+	
+	public Page<Categoria> findPage(Integer page, Integer linePerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linePerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
+	}
+	
 }
