b753af422af02b9ba36769d1cc6f7ffa4e620db6
= <=>
2020-01-10 16:31:53 -0300

    Added Delete

--- src/main/java/com/mugnol/cursomc/services/CategoriaService.java
+++ src/main/java/com/mugnol/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.mugnol.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.mugnol.cursomc.domain.Categoria;
 import com.mugnol.cursomc.repositories.CategoriaRepository;
+import com.mugnol.cursomc.services.exception.DataIntegrityException;
 import com.mugnol.cursomc.services.exception.ObjectNotFoundException;
 
 
@@ -30,6 +32,17 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId()); // Check exists
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id); // Check exists
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos.");
+		}
+		
 	}
 
 }
